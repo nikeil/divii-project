c13dcd89567b902a22db14a695f46bfff2b7ba11
nikeil <118449797+nikeil@users.noreply.github.com>
2026-06-18 00:06:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 function scrollToSection(id){
-document
-.getElementById(id)
-.scrollIntoView({
-behavior:"smooth"
-});
+    document
+    .getElementById(id)
+    .scrollIntoView({
+        behavior:"smooth"
+    });
 }
 
 function calculateCompatibility(){
+    const score =
+    Math.floor(Math.random()*15)+85;
 
-const score =
-Math.floor(Math.random()*15)+85;
-
-document
-.getElementById("score")
-.innerText = score + "%";
+    document
+    .getElementById("score")
+    .innerText = score + "%";
 }
-function saveSurvey() {
-    alert("Survey submitted successfully!");
-    scrollToSection("compatibility");
+
+function saveSurvey(){
+    alert("Survey saved!");
+    document
+    .querySelector(".compatibility")
+    .scrollIntoView({
+        behavior:"smooth"
+    });
+}
+
+function saveWedding(answer){
+    alert("Answer selected: " + answer);
 }
